6bbeebfe5bd5e6ed6cf51dc7c080e5d0ed60768d
Ersoy Efe Uruk <efeuruk1@gmail.com>
2020-08-25 01:18:05 +0300

    Hamburger menu implementation

---
+++ src/components/Landing/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Navbar";
+
+export default function Landing() {
+  return (
+    <div className="landing">
+      <Navbar />
+      <h1>Antalya Kartuş</h1>
+      <p>
+        Yıllardır Kartuşun <em>Tek</em> Adresi
+      </p>
+    </div>
+  );
+}
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+function List({ className }) {
+  return (
+    <ul className={className}>
+      <li>
+        <a className="navbar__link" href="#">
+          Anasayfa
+        </a>
+      </li>
+      <li>
+        <a className="navbar__link" href="#about-us">
+          Hakkımızda
+        </a>
+      </li>
+      <li>
+        <a className="navbar__link" href="#gallery">
+          Galeri
+        </a>
+      </li>
+      <li>
+        <a className="navbar__link" href="#contact-us">
+          Bize Ulaşın
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export function HamburgerButton({ openHamburger, open }) {
+  return (
+    <button className="hamburger-button" onClick={() => openHamburger()}>
+      <i className={`fas fa-${open ? "arrow-left" : "bars"}`}></i>
+    </button>
+  );
+}
+
+export function HamburgerMenu({ open }) {
+  return (
+    <div className={`hamburger-menu${open ? " active" : ""}`}>
+      <List />
+      <p className="copyright">Bütün haklar Antalya Kartuşa aittir. &copy;</p>
+    </div>
+  );
+}
+
+export default function Navbar() {
+  const [openHamburger, setOpenHamburger] = useState(false);
+
+  const clickHandler = () => {
+    setOpenHamburger(!openHamburger);
+  };
+
+  return (
+    <>
+      <HamburgerButton openHamburger={clickHandler} open={openHamburger} />
+      <HamburgerMenu open={openHamburger} />
+      <List className="navbar" />
+    </>
+  );
+}
